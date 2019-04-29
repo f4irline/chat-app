4662520272b9ba77ea28f413c099df9b644c18c3
Tommi Lepola <lepola.tommi@gmail.com>
2019-04-29 07:58:36 +0300

    Chat functionalities work with basic styling.

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewChecked, OnDestroy } from '@angular/core';
 import { Message } from 'src/app/models/Message';
 import { SocketIoService } from 'src/app/services';
-import { Observable, Subscription } from 'rxjs';
 import { Typing } from 'src/app/models/Typing';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked, OnDestroy {
 
   @Input() userName: string;
 
   typing: Typing;
-
   msg: Message;
   messages: Message[];
+
+  msgSubscription$: Subscription;
+  clearSubscription$: Subscription;
+  typingSubscription$: Subscription;
 
   constructor(private socketIo: SocketIoService) {
     this.msg = {
@@ -43,26 +46,36 @@ export class ChatComponent implements OnInit {
     };
   }
 
+  ngOnDestroy() {
+    this.msgSubscription$.unsubscribe();
+    this.clearSubscription$.unsubscribe();
+    this.typingSubscription$.unsubscribe();
+  }
+
   ngOnInit() {
-    const chatContainer = document.querySelector('.chat-container');
-    chatContainer.scrollTop = chatContainer.scrollHeight - chatContainer.clientHeight;
+    this.scrollToBottom();
 
     this.msg.userName = this.userName;
-    this.socketIo.message.subscribe((msg) => {
+
+    this.msgSubscription$ = this.socketIo.message.subscribe((msg) => {
       this.messages.push(msg);
     });
 
-    this.socketIo.clear.subscribe(() => {
+    this.clearSubscription$ = this.socketIo.clear.subscribe(() => {
       this.typing = {
         userName: undefined,
       };
     });
 
-    this.socketIo.typing.subscribe((name) => {
+    this.typingSubscription$ = this.socketIo.typing.subscribe((name) => {
       this.typing = {
         userName: name,
       };
     });
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
   }
 
   msgChanged(event: any) {
@@ -78,5 +91,10 @@ export class ChatComponent implements OnInit {
   send() {
     this.socketIo.sendMessage(this.msg);
     this.msg.msg = '';
+  }
+
+  scrollToBottom() {
+    const chatContainer = document.querySelector('.chat-container');
+    chatContainer.scrollTop = chatContainer.scrollHeight - chatContainer.clientHeight;
   }
 }
