3eb0420344967dc218120c437dfb764ab3a40421
Tommi Lepola <lepola.tommi@gmail.com>
2019-05-21 20:46:19 +0300

    Fix issue of sending incorrect empty username to socket io

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy, OnInit, Output, EventEmitter, } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { User, UserDetails } from '../../models';
 import { AppState } from '../../store';
+import { SocketIoService } from '../../services';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidebarComponent implements OnInit {
-  @Input() users: User;
+export class SidebarComponent implements OnInit, OnDestroy {
   @Output() pm = new EventEmitter<string>();
+
+  usersSubscription$: Subscription;
   userDetails$: Observable<UserDetails>;
+  users: User[];
 
   constructor(
     private store: Store<AppState>,
+    private ref: ChangeDetectorRef,
+    private socketIo: SocketIoService,
   ) {
     this.userDetails$ = this.store.select('userDetails');
   }
 
+  ngOnDestroy() {
+    this.usersSubscription$.unsubscribe();
+  }
+
   ngOnInit() {
+    this.usersSubscription$ = this.socketIo.users.subscribe((users) => {
+      this.users = users;
+      this.ref.markForCheck();
+    });
   }
 
   startPm(user: User) {
