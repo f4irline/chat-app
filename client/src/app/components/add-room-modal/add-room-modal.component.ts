10c71c1df8a1614372b230aaaa5e8ae49728ad35
Tommi Lepola <lepola.tommi@gmail.com>
2019-05-21 21:06:09 +0300

    Add indication for user that adding room failed

--- client/src/app/components/add-room-modal/add-room-modal.component.ts
+++ client/src/app/components/add-room-modal/add-room-modal.component.ts
@@ -1,18 +1,26 @@
 import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   OnInit,
   ViewChild,
   EventEmitter,
   ElementRef,
-  Output } from '@angular/core';
+  Output,
+  } from '@angular/core';
 
+import { Fade } from '../../animations/Fade';
 import { Room } from '../../models';
 import { ApiService } from '../../services';
 
 @Component({
   selector: 'app-room-modal',
   templateUrl: './add-room-modal.component.html',
-  styleUrls: ['./add-room-modal.component.scss']
+  styleUrls: ['./add-room-modal.component.scss'],
+  animations: [
+    Fade,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RoomModalComponent implements OnInit {
 
@@ -21,8 +29,12 @@ export class RoomModalComponent implements OnInit {
 
   @Output() joinRoom = new EventEmitter<number>();
 
+  addFailed = false;
+  errorTimeout: number;
+
   constructor(
     private apiService: ApiService,
+    private ref: ChangeDetectorRef,
     ) { }
 
   ngOnInit() {
@@ -32,10 +44,30 @@ export class RoomModalComponent implements OnInit {
   }
 
   saveRoom() {
-    this.apiService.saveRoom(this.room).subscribe((room) => {
-      this.room.roomName = '';
-      this.joinRoom.emit(room.id);
-      this.closeBtn.nativeElement.click();
-    });
+    this.apiService.saveRoom(this.room).subscribe(
+      (room) => this.handleSuccess(room),
+      (err) => this.handleError(err),
+    );
+  }
+
+  handleSuccess(room: Room) {
+    this.room.roomName = '';
+    this.joinRoom.emit(room.id);
+    this.closeBtn.nativeElement.click();
+  }
+
+  handleError(err) {
+    this.addFailed = true;
+    this.ref.markForCheck();
+
+    if (this.errorTimeout) {
+      window.clearTimeout(this.errorTimeout);
+    }
+
+    this.errorTimeout = window.setTimeout(() => {
+      this.addFailed = false;
+      this.errorTimeout = undefined;
+      this.ref.markForCheck();
+    }, 4000);
   }
 }
