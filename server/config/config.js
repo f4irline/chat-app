3e9985ef72ae33511cf5f05812f2e7ecc97c0876
Tommi Lepola <lepola.tommi@gmail.com>
2019-05-22 18:46:16 +0300

    Add optionality for mysql configuration (since they are not needed neccessarily)

--- server/config/config.js
+++ server/config/config.js
@@ -4,10 +4,10 @@ const config = {};
  * For MySQL installation (not used currently).
  */
 config.mysql = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
+    host: process.env.DB_HOST | undefined,
+    user: process.env.DB_USER | undefined,
+    password: process.env.DB_PASSWORD | undefined,
+    database: process.env.DB | undefined,
 }
 
 config.port = process.env.PORT;
